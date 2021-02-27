d5eb8ed20694521afcd1f06b52db115a4de5c068
leoorodr <leleu45_melo@hotmail.com>
2021-02-26 21:25:51 -0300

    Criação do server

--- server/corsConfig.js
+++ server/corsConfig.js
@@ -1,8 +1,13 @@
+// Adicionando o pacote que resolve o problema de cors da API.
 const cors = require('cors')
 
 module.exports = (app) => {
+    // Resolvendo o problema de cors no site
     app.use((req, res, next) => {
+        // Qual site tem permissão de realizar a conexão com o site
+        // O "*" define que qualquer site tem pode usar a API.
         res.header("Access-Control-Allow-Origin", "*");
+        // Métodos que a conexão pode realizar na API.
         res.header("Access-Control-Allow-Methods", 'GET, PUT, POST, DELETE');
 
         app.use(cors());
