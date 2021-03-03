6cc8ba1b0c12f59078ec8103c7b7db71e94d8570
Vicente1507 <vicente.victor1507@gmail.com>
2021-03-03 17:29:27 -0300

    update

--- server/api/maquinas_get.js
+++ server/api/maquinas_get.js
@@ -1,5 +1,41 @@
-module.exports = (req, res, next) => {
-    res.status(200).send({
-        mensagem: "Get funcionou"
-    });
+const dbCon = require("../db.js");
+
+const filtrarImagens = (imagens, id) => (
+    imagens.filter(({ maquina_id }) => maquina_id === id).map(({ caminho, nome }) => ({ caminho, titulo: nome }))
+);
+
+module.exports = (req, res) => {
+    try {
+        dbCon.query('SELECT * FROM maquina',
+            (erroMaquina, maquina) => {
+                if (erroMaquina) {
+                    const erro = JSON.stringify({
+                        cod: 502, mensagem: "Erro ao buscar dados das máquinas!"
+                    });
+                    throw new Error(erro);
+                }
+
+                dbCon.query('SELECT * FROM midias',
+                    (erroMidias, midias) => {
+                        if (erroMidias) {
+                            const erro = JSON.stringify({
+                                cod: 502, mensagem: "Erro ao buscar dados das mídias!"
+                            });
+                            throw new Error(erro);
+
+                        }
+                        const dados =
+                        maquina.map((item) => {
+                            return {
+                                ...item,
+                                midias: filtrarImagens(midias, item.id)
+                            }
+                        });
+                        res.status(200).send(maquina)
+                    })
+            })
+    } catch (erro) {
+
+    }
+
 }
