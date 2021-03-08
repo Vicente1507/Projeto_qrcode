1332d11a6bd368fd2bd5e1799f5be1a97766900c
Vicente1507 <vicente.victor1507@gmail.com>
2021-03-08 17:59:20 -0300

    update

--- server/api/maquinas_post.js
+++ server/api/maquinas_post.js
@@ -1,5 +1,32 @@
+const dbCon = require('../db.js');
+
 module.exports = (req, res, next) => {
-    res.status(201).send({
-        mensagem: "Post funcionou"
-    });
+    try{
+        const {nome, descricao} = req.body;
+
+        if(!nome || !descricao){
+            const erro = JSON.stringify({ cod: 400, mensagem: 'Dados incompletos!' });
+            throw new Error(erro);
+        }
+
+        dbCon.query(`INSERT INTO maquina (id, nome, descricao) VALUES (null, '${nome})', '${descricao}')`,
+
+        (erroMaquinas, resMaquina) => {
+
+            if (erroMaquinas){
+                const erro = JSON.stringify({ cod: 502, mensagem:'Erro ao inserir dados na tabela maquinas!' });
+                throw new Error(erro);
+            }
+
+            res.status(201).send({ status: "Sucesso", mensagem: "Dados adicionados com sucesso!" });
+        });
+
+    }catch ({ message }) {
+        const { cod, mensagem} = JSON.parse(message)
+        res.status(cod).send({ status:'Falha', mensagem });
+    }
 }
+
+// res.status(201).send({
+//     mensagem: "Post funcionou"
+// });
